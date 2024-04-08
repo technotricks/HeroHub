8250c8a51064042bd3e48cf216bd28fcff448b06
Kishore <kishoreraj87@yahoo.com>
2024-04-08 10:46:23 +0800

    -- Status bar

--- src/components/template/SafeScreen/SafeScreen.tsx
+++ src/components/template/SafeScreen/SafeScreen.tsx
@@ -6,7 +6,7 @@ import {MainContainer} from '@/components/atom';
 function SafeScreen({children}: PropsWithChildren) {
   return (
     <MainContainer>
-      <StatusBar barStyle={'light-content'} backgroundColor={'red'} />
+      <StatusBar barStyle={'default'} backgroundColor="lightgreen" />
       {children}
     </MainContainer>
   );
